6c6dda949dc03289e7b694b759f1af2d5c5314b6
Aaron Williams <aaron.colby.williams@gmail.com>
2015-06-23 11:07:31 -0700

    added codrops medium transition code

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,9 +1,190 @@
 'use strict';
 
-var App = App || {};
+jQuery.ajaxSetup({ cache: false });
 
-App = {
-  init: function () {
-    console.log('The Voice of the West. The Voice of San Francisco and the Bay Area.');
-  }
+var App = App || {
+  canScroll:          true,
+  initialLoad:        true,
+  animationDuration:  500,
+  postCount:          5,
+  currentPostIndex:   1,
+  postCache:          {},
+  pageTemplate:       null,
 };
+
+App.load = function(){
+  this.currentPostIndex = getURLIndex();
+  this.makeSelections();
+
+  $body.append( this.$current );
+  $body.append( this.$next );
+
+  var self = this;
+  this.createPost({ type: 'current' }, function(){
+    self.createPost({ type: 'next' }, function(){
+
+      /* Selections. */
+      self.refreshCurrentAndNextSelection();
+
+      /* Push initial on to stack */
+      history.pushState(pageState(), "", "#" + self.currentPostIndex)
+
+      /* Bind to some events. */
+      self.bindGotoNextClick();
+      self.bindPopstate();
+      self.bindWindowScroll();
+    })
+  })
+}
+
+App.makeSelections = function(){
+  this.$page         = $('.page');
+  this.pageTemplate  = elementToTemplate( this.$page.clone() );
+  this.$current      = this.currentElementClone();
+  this.$next         = this.nextElementClone();
+}
+
+App.getPost = function(index, callback){
+  callback = callback || $.noop;
+
+  if ( this.postCache[index] ){
+    callback( this.postCache[index] );
+    return;
+  }
+
+  var self = this;
+  $.getJSON('/static/stories/post_'+ index +'.json', function(d){
+    self.postCache[index] = d;
+    callback(d)
+  });
+}
+
+App.nextPostIndex = function(index){
+  return (index === this.postCount) ? 1 : index + 1
+}
+
+App.createPost = function(opts, callback){
+  opts      = opts || {};
+  var self  = this;
+  var type  = opts['type'] || 'next';
+
+  if ( opts['fromTemplate'] ){
+    $body.append( this.nextElementClone() );
+    this['$' + type] = $('.' + type)
+  }
+
+  var index = (type == 'next') ? this.nextPostIndex( this.currentPostIndex) : this.currentPostIndex;
+  this.getPost(index, function(d){
+    self.contentizeElement(self['$' + type], d);
+    callback && callback();
+  });
+
+}
+
+App.contentizeElement = function($el, d){
+  $el.find('.big-image').css({ backgroundImage: "url(" + d.image + ")" });
+  $el.find('h1.title').html(d.title);
+  $el.find('h2.description').html(d.title_secondary);
+  $el.find('.content .text').html(d.content);
+  $el.find('h3.byline time').html(d.date);
+  $el.find('h3.byline .author').html(d.author);
+}
+
+App.animatePage = function(callback){
+  var self              = this;
+  var translationValue  = this.$next.get(0).getBoundingClientRect().top;
+  this.canScroll        = false;
+
+  this.$current.addClass('fade-up-out');
+
+  this.$next.removeClass('content-hidden next')
+       .addClass('easing-upward')
+       .css({ "transform": "translate3d(0, -"+ translationValue +"px, 0)" });
+
+  setTimeout(function(){
+      scrollTop();
+      self.$next.removeClass('easing-upward')
+      self.$current.remove();
+
+      self.$next.css({ "transform": "" });
+      self.$current = self.$next.addClass('current');
+
+      self.canScroll = true;
+      self.currentPostIndex = self.nextPostIndex( self.currentPostIndex );
+
+      callback();
+  }, self.animationDuration );
+}
+
+App.bindGotoNextClick = function(){
+  var self  = this;
+  var e     = 'ontouchstart' in window ? 'touchstart' : 'click';
+
+  this.$next.find('.big-image').on(e, function(e){
+    e.preventDefault();
+    $(this).unbind(e);
+
+    self.animatePage(function(){
+      self.createPost({ fromTemplate: true, type: 'next' });
+      self.bindGotoNextClick();
+      history.pushState( pageState(), '', "#" + self.currentPostIndex);
+    });
+  });
+}
+
+App.bindPopstate = function(){
+  var self = this;
+  $window.on('popstate', function(e){
+
+    if( !history.state || self.initialLoad ){
+      self.initialLoad = false;
+      return;
+    }
+
+    self.currentPostIndex = history.state.index;
+    self.$current.replaceWith( history.state.current );
+    self.$next.replaceWith( history.state.next );
+
+    self.refreshCurrentAndNextSelection();
+    self.createPost({ type: 'next' });
+    self.bindGotoNextClick();
+  });
+}
+
+App.bindWindowScroll = function(){
+  var self = this;
+  $window.on('mousewheel', function(ev){
+    if ( !self.canScroll )
+      ev.preventDefault()
+  })
+}
+
+App.refreshCurrentAndNextSelection = function(){
+  this.$current      = $('.page.current');
+  this.$next         = $('.page.next');
+}
+
+App.nextElementClone = function(){
+  return this.$page.clone().removeClass('hidden').addClass('next content-hidden');
+}
+
+App.currentElementClone = function(){
+  return this.$page.clone().removeClass('hidden').addClass('current');
+}
+
+
+function elementToTemplate($element){
+  return $element.get(0).outerHTML;
+}
+
+function scrollTop(){
+  $body.add($html).scrollTop(0);
+}
+
+function pageState(){
+  return { index: App.currentPostIndex, current: elementToTemplate(App.$current), next: elementToTemplate(App.$next) }
+}
+
+function getURLIndex(){
+  return parseInt( (history.state && history.state.index) ||window.location.hash.replace('#', "") || App.currentPostIndex );
+}
